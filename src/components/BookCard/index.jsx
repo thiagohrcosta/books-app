59aa5d95292417707a7c06f38abae786edef1e78
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-09-14 16:44:39 -0300

    Adding book componets

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -1,36 +1,67 @@
-import { Box, Container, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Container, Image, Text } from "@chakra-ui/react";
 
-export default function BookCard({key, title, author, cover}) {
+export default function BookCard({
+  key,
+  title,
+  year,
+  description,
+  author,
+  cover
+}) {
   return (
     <Container
       margin={'50px auto'}
     >
       <Box
-        maxW='sm'
         borderWidth='1px'
         bgColor={'gray.100'}
         borderRadius='lg'
-        width={400}
+        width={500}
         height={300}
         display='flex'
       >
-        <Image
-          src={cover}
-          alt={"Cover"}
-          marginTop={-10}
-          paddingLeft={2}
-          width={200}
-        />
+        <Box>
+          <Image
+            src={cover}
+            alt={"Cover"}
+            marginTop={-10}
+            marginLeft={2}
+            marginBottom={10}
+            height={300}
+            minWidth={200}
+            boxShadow={'lg'}
+          />
+        </Box>
         <Box
           padding={4}
         >
-          <Text
-            color={'gray.800'}
-            fontWeight='bold'
-            textTransform={'uppercase'}
+          <Box
+            display='flex'
+            flexDirection='column'
           >
-            {author}
+            <Text>
+              {title}
+            </Text>
+            <Text
+              color={'gray.800'}
+              fontWeight='bold'
+              textTransform={'uppercase'}
+            >
+              by {author}
+            </Text>
+            <Text>
+              ({year})
+            </Text>
+          </Box>
+          <Text
+          >
+            {description.substring(0, 120)}...
           </Text>
+          <Button
+              colorScheme='blue'
+              width={100}
+              margin={'10px 0px'}
+            >More</Button>
         </Box>
       </Box>
 
