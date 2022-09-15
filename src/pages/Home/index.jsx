b3e72243b3365082e3e22a95b68ea99e33477e64
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-09-14 22:52:04 -0300

    Adding new routes and darkmode

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import Books from "../../components/Books/Books";
+import HomeBanner from "../../components/HomeBanner";
+
+export default function Home() {
+  return (
+    <>
+      <HomeBanner />
+      <Books />
+    </>
+  )
+}
