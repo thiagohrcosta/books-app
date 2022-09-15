4d9f9cf5bfc63453696ddee297e5852cf15169e8
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-09-14 23:24:12 -0300

    Adding banner

--- src/components/HomeBanner/index.jsx
+++ src/components/HomeBanner/index.jsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 export default function HomeBanner() {
   return (
     <Box
-      backgroundImage={"https://res.cloudinary.com/dloadb2bx/image/upload/v1663204672/dark_ev9lll.jpg"}
+      backgroundImage={"https://res.cloudinary.com/dloadb2bx/image/upload/v1663208632/st_ellft1.jpg"}
       backgroundRepeat="no-repeat"
       backgroundSize={'100% 100%'}
       height={450}
